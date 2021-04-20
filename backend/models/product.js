63163dae2ffb86c2c9730b1579536acb37076c03
lauracejas <lauracejasmartin94@gmail.com>
2021-04-20 10:41:23 -0400

    connect mysql and seeding data

---
+++ backend/models/product.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+{
+    _id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    size: {
+      type: DataTypes.STRING,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 const express = require("express");
+const session = require('express-session');
+// const mongoose = require("mongoose");
+const routes = require("./backend/routes");
+// const config = require("./backend/config");
+// const data = require("./backend/scripts/seedDB")
+
+const sequelize = require('./backend/config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+ 
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// const mongodbUrl = data.MONGODB_URI;
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/products", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// });
+
+app.use(routes);
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("/api/products", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
