6618b17fea583f0f76890f685bf8fc80eccb1aa7
Nelson A Escalante <76067715+nescalante92@users.noreply.github.com>
2021-04-21 13:19:45 -0400

    Nelson003 (#8)
    * added font-end files and bootstrap
    
    * updated homepage and login

---
+++ client/src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import "./style.css";
+
+
+const Banner = () => {
+    return (
+        <Jumbotron fluid>
+            <Container>
+                <h1>Welcome to CLAN PRINTS! </h1>
+                <p>
+                
+                </p>
+            </Container>
+        </Jumbotron>
+    )
+}
+
+export default Banner;
+
---
+++ client/src/components/HotCards/HotCards.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import "./style.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const HotCards = () => {
+    return (
+        //--------------------------First Hot Card----------------------
+        <Container fluid>
+            <Row className="cardrow">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+            </Row>
+        </Container>
+        
+    )
+}
+
+export default HotCards;
