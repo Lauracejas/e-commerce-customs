66f4d377a37293f9f21c4f20eeebf5cc88ae3646
Andrew Coon <thenran@gmail.com>
2021-04-21 14:15:06 -0400

    Adjusted user routes, previous login check route now creates user. Added new /login route for logging in.

--- backend/routes/api/userRoute.js
+++ backend/routes/api/userRoute.js
@@ -19,14 +19,41 @@ router.get("/createadmin", async (req, res) => {
 })
 
 // Checks for logged in session
-router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect("/");
-        return;
-    }
-    res.render('login');
-})
+router.post("/", async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+        });
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        const validPass = await userData.checkPassword(req.body.password);
+
+        if (!userData || !validPass) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData._id;
+            req.session.logged_in = true;
+        });
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+})
 
 module.exports = router;
 
