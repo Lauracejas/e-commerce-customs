d9001e1fd4e0523033f70b276b76dd55b74f0557
Carlos Abril <carlos.abril87@outlook.com>
2021-04-22 14:40:56 -0400

    single product on page

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,24 +1,34 @@
-import { useParams } from "react-router";
+
 import { getProduct } from "../utils/API";
-import React, {useEffect} from "react"
+import React, { useEffect, useState } from "react"
+import Row from "react-bootstrap/Row"
 import HotCards from "../components/HotCards/HotCards";
 
 
 
 
-const ProductPage = () => {
-    const {id} = useParams();
 
+const ProductPage = (props) => {
+    const [product, setProduct] = useState({});
+    const [products, setProducts] = useState([]);
     useEffect(() => {
-        getProduct(id)
-        .then(product => console.log(product.data))
-        
-    }, [id])
-
-    console.log(id)
+        getProduct(props.location.state.id)
+            .then(product => {
+                console.log(product.data);
+                setProduct(product.data);
+            }
+            )
+    }, [])
+    console.log(product)
     return (
         <>
-      
+        <div className="container-fluid main-contain">
+            <Row className="apiFlex">
+
+            <HotCards product={product} />
+            </Row>
+
+        </div>
         </>
     )
 }
