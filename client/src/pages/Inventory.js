c6a528dd995529f6c4e3726abefc1a232bb19030
Andrew Coon <thenran@gmail.com>
2021-04-23 13:31:07 -0400

    Built out table for Inventory page

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,13 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
-import HotCards from "../components/HotCards/HotCards";
+import Table from "react-bootstrap/Table";
+import SearchBar from "../components/SearchBar/SearchBar";
+import TableRow from "../components/TableRow";
+import { getAllProducts, getProduct } from "../utils/API";
 
 const Inventory = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProducts()
+      .then(product => {
+        console.log(product.data);
+      })
+  }, []);
 
   return (
-    <Row>
+    <div className="container-fluid main-contain">
+      <SearchBar />
 
-    </Row>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${products.map(product => <TableRow prop={product} />)}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
