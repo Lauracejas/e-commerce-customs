4001d8dc9d7a63a51c9f887e565d85b3972cd797
Andrew Coon <thenran@gmail.com>
2021-04-23 13:34:00 -0400

    Created TableRow component

---
+++ client/src/components/TableRow/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TableRow = (product) => {
+  return (
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  );
+}
+
+export default TableRow;
