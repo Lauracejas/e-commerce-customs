49fdabd36c1dd3d12662feb5bf9a3994d50c66b5
Andrew Coon <thenran@gmail.com>
2021-04-23 10:35:37 -0400

    Begun Inventory page, imported expected resources, added call to retrieve product data and basic frame for page.

---
+++ client/src/pages/Inventory.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import HotCards from "../components/HotCards/HotCards";
+
+const Inventory = () => {
+
+  return (
+    <Row>
+
+    </Row>
+  );
+}
+
+export default Inventory;
