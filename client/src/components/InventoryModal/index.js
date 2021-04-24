e7209a832d1a32a766b9f04a2fff8272efccf29c
Andrew Coon <thenran@gmail.com>
2021-04-24 14:19:19 -0400

    Added modal submit handler, added states for data

--- client/src/components/InventoryModal/index.js
+++ client/src/components/InventoryModal/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
@@ -7,9 +7,92 @@ import FormControl from 'react-bootstrap/FormControl';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const InventoryModal = props => {
+const InventoryModal = ({props}) => {
+  const [product, setProduct] = useState();
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [size, setSize] = useState();
+  const [color, setColor] = useState();
+  const [description, setDescription] = useState();
+  const [count, setCount] = useState();
+
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    axios.put("/api/products/addUpdate", {
+      name: name,
+      price: price,
+      size: size,
+      color: color,
+      description: description,
+      countInStock: count
+    })
+    .then(res => {
+      if (res.data.message);
+      console.log(res);
+    });
+  }
+
   return (
-    ''
+    <Modal.Dialog 
+        >
+            <Modal.Header
+            closeButton 
+            style={{
+                display: "inline-block",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+             
+                <Modal.Title >Add/Update Inventory Item</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body className="text-left">
+                <p><strong>E-mail</strong></p>
+                <InputGroup size={"md"} className="mb-3">
+                    <FormControl
+                        className="email-login"
+                        type="email"
+                        placeholder="email"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </InputGroup>
+                <p><strong>Password</strong></p>
+                <InputGroup size={"md"} className="mb-3">
+                    <FormControl
+                        className="password-login"
+                        type="password"
+                        placeholder="Password"
+                        aria-label="Recipient's password"
+                        aria-describedby="basic-addon2"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </InputGroup>
+
+            <Row style={{
+                marginBottom:"20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+
+                <Button
+                    onClick={handleSubmit}
+                    variant="warning"
+                >Login</Button>
+            </Row>
+            </Modal.Body>
+
+            <Modal.Footer>
+                <p>Are you new in reVamped?</p>
+                <Link to="/register"><Button  variant="primary">Create your account</Button></Link>
+
+            </Modal.Footer>
+        </Modal.Dialog>
   );
 }
 
