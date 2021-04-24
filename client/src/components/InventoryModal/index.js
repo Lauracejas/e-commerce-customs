676b688089eb6717a4b20d0810644a1f973c4734
Andrew Coon <thenran@gmail.com>
2021-04-24 11:27:07 -0400

    Created InventoryModal component

---
+++ client/src/components/InventoryModal/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const InventoryModal = props => {
+  return (
+    ''
+  );
+}
+
+export default InventoryModal;
