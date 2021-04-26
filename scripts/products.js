296be80761dd8e7d09de2674b0268b3126d8820d
Nelson Escalante <nescalante92@gmail.com>
2021-04-26 02:06:21 -0400

    added mens pants

--- scripts/products.js
+++ scripts/products.js
@@ -5,9 +5,9 @@
         _id: 1,
         name: "Alex Vando Mens Dress Shirts Wrinkle Free Regular Fit Stretch Bamboo Men Shirt",
         image: '/images/m.dress.shirts/black.jpeg',
-        price: 25,
+        price: 24,
         size: "L",
-        color: "black",
+        color: "Black",
         countInStock: 20,
         description: "The material is comfortable, breathable and not see through. It has a little spandex mix in so it stretches some which allows for some moving room"
     },
@@ -17,7 +17,7 @@
         image: '/images/m.dress.shirts/blue.jpg',
         price: 25,
         size: "M",
-        color: "light blue",
+        color: "Light Blue",
         countInStock: 20,
         description: "DESIGN: Perfect fit, one chest pocket, turn down collar, this long sleeve button down dress shirt characterized by plaid pattern on collar, sleeves and front, modern and stylish"
     },
@@ -25,9 +25,9 @@
         _id: 3,
         name: "Alimens & Gentle French Cuff Regular Fit Dress Shirts (Cufflink Included)",
         image: '/images/m.dress.shirts/cyan.jpg',
-        price: 20,
+        price: 35,
         size: "M",
-        color: "cyan",
+        color: "Cyan",
         countInStock: 20,
         description: "OCCASIONS: This stylish mens button down shirts can be worn tucked or untucked pairing, are perfect for Party, Business, Work, Holiday, Travel, Casual, or Daily Life/ you can match with Flat Front, Chino Pants, Jeans, Cargo Pants."
     },
@@ -37,7 +37,7 @@
         image: '/images/m.dress.shirts/purple.jpeg',
         price: 30,
         size: "L",
-        color: "purple",
+        color: "Purple",
         countInStock: 20,
         description: "SIZE: Size are US standard size, please choose your size according to our size chart (Not the Amazon Size Chart) and if you have big belley and wide shoulders, we recommend you to go up one size."
     },
@@ -47,7 +47,7 @@
         image: '/images/m.dress.shirts/red.jpg',
         price: 30,
         size: "L",
-        color: "red",
+        color: "Red",
         countInStock: 20,
         description: "FABRIC: Wicking cotton fabric helps evaporate moisture, offers you great hand feel, skin friendly, very comfortable to wear."
     },
@@ -55,9 +55,9 @@
         _id: 6,
         name: "Jack Martin - Club/Penny Collar - Blue Bengal Stripe Slim Fit Shirt. Mens 1920s Blinders Gang Shirts",
         image: '/images/m.dress.shirts/redstripe.jpg',
-        price: 30,
+        price: 45,
         size: "L",
-        color: "white",
+        color: "White",
         countInStock: 20,
         description: "GUARANTEE: Suitable for hand and machine wash at 30 degrees, hold up well after washing. Kindly wash it before wearing."
     },
@@ -67,7 +67,7 @@
         image: '/images/m.dress.shirts/white.jpeg',
         price: 30,
         size: "L",
-        color: "white",
+        color: "White",
         countInStock: 20,
         description: "US STANDARD SIZE: Our dress shirts are made based on USA standard size. This business shirt is regular fit cut."
     },
@@ -75,84 +75,84 @@
         //------------------------Men's Pant--------------------------------
 
         _id: 8,
-        name: "Men's Pants",
+        name: "Men's Expandable Waist Classic-Fit Flat-Front Dress Pants",
         image: "/images/m.pants/bluestripe.jpg",
         price: 30,
         size: "L",
-        color: "navy",
+        color: "Navy",
         countInStock: 20,
-        description: "d"
+        description: "An expandable waist guarantees a perfect fit in these classic-fit dress pants with flat-front styling."
     },
     {
         _id: 9,
-        name: "Men's Pants",
+        name: "Perry Ellis Men's Modern Fit Performance Pant",
         image: "/images/m.pants/class.jpeg",
-        price: 25,
-        size: "S",
-        color: "grey",
-        countInStock: 20,
-        description: "d"
-    },
-    {
-        _id: 10,
-        name: "Men's Pants",
-        image: "/images/m.pants/grey.jpg",
-        price: 25,
-        size: "S",
-        color: "grey",
-        countInStock: 20,
-        description: "d"
-    },
-    {
-        _id: 11,
-        name: "Men's Pants",
-        image: "/images/m.pants/navy.jpg",
-        price: 25,
-        size: "S",
-        color: "blue",
-        countInStock: 20,
-        description: "d"
-    },
-    {
-        _id: 12,
-        name: "Men's Pants",
-        image: "/images/m.pants/red.jpg",
-        price: 25,
-        size: "L",
-        color: "Red",
-        countInStock: 20,
-        description: "d"
-    },
-    {
-        _id: 13,
-        name: "Men's Pants",
-        image: "/images/m.pants/style.jpeg",
-        price: 25,
-        size: "L",
-        color: "Grey",
-        countInStock: 20,
-        description: "d"
-    },
-    {
-        _id: 14,
-        name: "Men's Pants",
-        image: "/images/m.pants/tan.png",
-        price: 25,
-        size: "L",
-        color: "Tan",
-        countInStock: 20,
-        description: "d"
-    },
-    //---------------Men's Shoes------------
-    {
-        _id: 15,
-        name: "Men's Shoes",
-        image: "/images/m.shoes/black.jpg",
         price: 25,
         size: "L",
         color: "Black",
         countInStock: 20,
-        description: "d"
+        description: "Modern fit – The sleek and classic look that you’ve been looking for. The modern fit pants sit at the waist with a trim fit through the hips and thighs and the pants feature a slightly-tapered leg opening."
+    },
+    {
+        _id: 10,
+        name: "Men's American Chino Flat Front Straight Fit Pant",
+        image: "/images/m.pants/grey.jpg",
+        price: 25,
+        size: "L",
+        color: "Grey",
+        countInStock: 20,
+        description: "Straight fit chino features a straight leg with zip fly and button closure.Wrinkle-free and machine washable to wear straight from the dryer."
+    },
+    {
+        _id: 11,
+        name: "Goodthreads Men's Slim-Fit Wrinkle-Free Comfort Stretch Dress Chino Pant",
+        image: "/images/m.pants/navy.jpg",
+        price: 50,
+        size: "L",
+        color: "Navy",
+        countInStock: 20,
+        description: "Wrinkle-free, easy-care flat front dress chino in a cotton twill with stretch elastane for comfort."
+    },
+    {
+        _id: 12,
+        name: "Classic-fit Wrinkle-Resistant Flat-Front Dress Pant fit by DXL",
+        image: "/images/m.pants/red.jpg",
+        price: 45,
+        size: "L",
+        color: "Red",
+        countInStock: 20,
+        description: "This classic-fit flat-front dress pant is crafted from a wrinkle-resistant 100% polyester and features a continuous comfort mechanism allowing the waistband to stretch up to four inches."
+    },
+    {
+        _id: 13,
+        name: "Calvin Klein Men's Slim Fit Dress Pant",
+        image: "/images/m.pants/style.jpeg",
+        price: 135,
+        size: "L",
+        color: "Grey",
+        countInStock: 20,
+        description: "Ready to wear. Arrives with a finished hemmed to avoid extra tailoring.Tailored around your body's natural silhouette."
+    },
+    {
+        _id: 14,
+        name: "Dockers Men's Classic Fit Easy Khaki Pants (Regular and Big & Tall)",
+        image: "/images/m.pants/tan.png",
+        price: 60,
+        size: "L",
+        color: "Tan",
+        countInStock: 20,
+        description: "Eased Fit Through Seat and Thigh, Sits at Waist With a Straight Leg.No Wrinkle Men's Khaki Pants With Stretch for Performance."
+    },
+    //---------------Men's Shoes------------
+    {
+        _id: 15,
+        name: "Bruno Marc Men's Formal Leather Lined Dress Shoes",
+        image: "/images/m.shoes/black.jpg",
+        price: 40,
+        size: "L",
+        color: "Black",
+        countInStock: 20,
+        description: "Breathable leather linings and leather-covered cushioned latex footbed for all-day comfort."
     },
     
 ]
