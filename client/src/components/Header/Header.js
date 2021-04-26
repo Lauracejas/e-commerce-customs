9d3fcc5f0a370183d8163def2d5882d152638559
Lauracejas <lauracejasmartin94@gmail.com>
2021-04-26 18:59:47 -0400

    Cart004 (#41)
    * added font-end files and bootstrap
    
    * main conflict
    
    * reviewed product.js
    
    * review
    
    * working on images
    
    * checking new progress
    
    * fixed log In and Sign In
    
    * finally fixed login issue
    
    Co-authored-by: Nelson Escalante <nescalante92@gmail.com>

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = (props) => {
                 <ul className="nav justify-content-center">
 
                     <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-                        {/* <img src={logo} alt='' /> */}
+                        <img src={logo} alt='' />
                       
                     </Link>
                 </ul>
@@ -63,7 +63,7 @@ const Header = (props) => {
                         </li>
                         {props.userLog?.user || props.user.name ? 
                             <li className="nav-item">
-                                <Link to="/logout"   className={location.pathname === "/cart" ? "nav-link active" : "nav-link"}>
+                                <Link to="/logout"   onClick={handleSubmit} className={location.pathname === "/cart" ? "nav-link active" : "nav-link"}>
                                     Logout
                                 </Link>
                                 {/* onChange={(e) => setLogged_in(e.target.value)} */}
