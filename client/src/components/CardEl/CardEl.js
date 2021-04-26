3b63497da355ff424430e8d0f857bf972b638e80
Carlos Abril <carlos.abril87@outlook.com>
2021-04-26 10:28:35 -0400

    logo

--- client/src/components/CardEl/CardEl.js
+++ client/src/components/CardEl/CardEl.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import "./styles.css"
 import Button from 'react-bootstrap/Button'
-import img from "../../assets/black.jpg"
+// import img from "../../assets/black.jpg"
 
 
 const CardEl = ({ product }) => {
   return (
     <>
       <div key={product._id} class="card" style={{ backgroundColor: "#00233A" }}>
-        <img src={img} class="card-img-top " alt="" />
+        <img  class="card-img-top " alt="" />
         <div class="card-body">
           <h5 class="card-title">{product.name}</h5>
           <p class="card-text">${product.price}</p>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import "./style.css";
 import axios from "axios"
-import logo from "../../public/images/logo.png"
+import logo from "../../Assets/logo.png"
 // import User from '../../../../models/user';
 
 const Header = (props) => {
